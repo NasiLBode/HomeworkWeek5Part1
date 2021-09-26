336cff163162c811a0d66cb9ed472def8e39ed07
Nasi Bode <nasibode@gmail.com>
2021-09-26 23:08:20 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace HomeworkWeek5Part1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.weightInput = new System.Windows.Forms.TextBox();
+            this.heightInput = new System.Windows.Forms.TextBox();
+            this.resultOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(289, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(111, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Weight (in lbs):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(71, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(324, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your Height (in Inches):";
+            // 
+            // weightInput
+            // 
+            this.weightInput.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.weightInput.Location = new System.Drawing.Point(421, 41);
+            this.weightInput.Name = "weightInput";
+            this.weightInput.Size = new System.Drawing.Size(235, 46);
+            this.weightInput.TabIndex = 3;
+            // 
+            // heightInput
+            // 
+            this.heightInput.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.heightInput.Location = new System.Drawing.Point(421, 130);
+            this.heightInput.Name = "heightInput";
+            this.heightInput.Size = new System.Drawing.Size(235, 46);
+            this.heightInput.TabIndex = 4;
+            // 
+            // resultOutput
+            // 
+            this.resultOutput.AutoSize = true;
+            this.resultOutput.Font = new System.Drawing.Font("Segoe UI Semibold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resultOutput.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.resultOutput.Location = new System.Drawing.Point(204, 354);
+            this.resultOutput.Name = "resultOutput";
+            this.resultOutput.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.resultOutput.Size = new System.Drawing.Size(0, 40);
+            this.resultOutput.TabIndex = 5;
+            this.resultOutput.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultOutput);
+            this.Controls.Add(this.heightInput);
+            this.Controls.Add(this.weightInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BMI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox weightInput;
+        private System.Windows.Forms.TextBox heightInput;
+        private System.Windows.Forms.Label resultOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿/* File Name: HomeworkWeek5Part1
+ * Programmer: Nasi Bode
+ * Problem Statement: Write a program (GUI) that calculates a person's BMI and prints a message
+ * telling them whether or not they are below, within, or above (blue, green, yellow, respectively)
+ * Use this formual: BMI = weight * 720 / height ^ 2
+ * Healthy range: 15 - 25
+ * Overall Plan: 
+ * 1. Create GUI
+ * 2. Create button for user to click
+ * 3. Create text box for user input (one for height, one for weight)
+ * 4. Label the text boxes
+ * 5. Convert string user input to int
+ * 6. Calculate BMI using formula
+ * 7. Create multi if else statement to output the result
+ * 8. If user is in healthy range display output in green to empty string
+ * 9. If user is below healthy range display output in blue to empty string
+ * 10.If user is above healthy range display output in red to empty string
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeworkWeek5Part1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int weight = int.Parse(weightInput.Text);
+            int height = int.Parse(heightInput.Text);
+            int BMI = (weight * 720) / (height * height);
+
+            if (BMI >= 15 && BMI <=25) //if user is in between 15 and 25
+            {
+                resultOutput.Text = "Your BMI of " + BMI.ToString() + " is within a healthy range";
+                resultOutput.ForeColor = Color.Green;
+                resultOutput.TextAlign = ContentAlignment.BottomCenter;
+            }
+            else if (BMI < 15) //if user is below 15
+            {
+                resultOutput.Text = "Your BMI of " + BMI.ToString() + " is BELOW a healthy range";
+                resultOutput.ForeColor = Color.Blue;
+                resultOutput.TextAlign = ContentAlignment.BottomCenter;
+            }
+            else //if user is above the healthy range
+            {
+                resultOutput.Text = "Your BMI of " + BMI.ToString() + " is ABOVE a healhty range";
+                resultOutput.ForeColor = Color.Yellow;
+                resultOutput.TextAlign = ContentAlignment.BottomCenter;
+            }
+
+        }
+    }
+}
